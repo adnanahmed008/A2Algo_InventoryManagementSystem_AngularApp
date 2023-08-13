83a4d358960172a90224eaeac64a2724e2ee771d
Adnan Ahmed <adnanahmed008@gmail.com>
2023-08-13 22:56:01 +0500

    Lazy loaded modules routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "/products" },
-  {
-    path: "products",
-    loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)
-  }
+  { path: "products", loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+  { path: "sales", loadChildren: () => import('./modules/sales/sales.module').then(m => m.SalesModule) },
+  { path: "purchases", loadChildren: () => import('./modules/purchases/purchases.module').then(m => m.PurchasesModule) },
+  { path: "", pathMatch: "full", redirectTo: "/products" }
 ];
 
 @NgModule({
